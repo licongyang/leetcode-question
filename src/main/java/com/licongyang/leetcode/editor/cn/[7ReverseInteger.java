27761f7c01fb0b640a657c2b924820c44fb87cd6
alfie <licongyang@mininglamp.com>
2022-07-10 09:23:37 +0800

    [feat] P7ReverseInteger 时间复杂度：O(lgx) 时间复杂度：O(1)

---
+++ src/main/java/com/licongyang/leetcode/editor/cn/[7ReverseInteger.java
@@ -0,0 +1,75 @@
+//给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。 
+//
+// 如果反转后整数超过 32 位的有符号整数的范围 [−2³¹, 231 − 1] ，就返回 0。 
+//假设环境不允许存储 64 位整数（有符号或无符号）。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 123
+//输出：321
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = -123
+//输出：-321
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 120
+//输出：21
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：x = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= x <= 2³¹ - 1 
+// 
+// Related Topics 数学 👍 3561 👎 0
+
+package com.licongyang.leetcode.editor.cn;
+
+//java:整数反转
+class P7ReverseInteger {
+    public static void main(String[] args) {
+        Solution solution = new P7ReverseInteger().new Solution();
+        int x = -5710;
+        System.out.println(solution.reverse(x));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int reverse(int x) {
+            int a = 0;
+            int b = 0;
+            while (x != 0) {
+                b = x % 10;
+                if (a > Integer.MAX_VALUE / 10 || (a == Integer.MAX_VALUE / 10 && b > Integer.MAX_VALUE % 10))
+                    return 0;
+                if (a < Integer.MIN_VALUE / 10 || (a == Integer.MIN_VALUE / 10 && b < Integer.MIN_VALUE % 10))
+                    return 0;
+                //判断条件不满足时才进行计算
+                a = a * 10 + b;
+                x = x / 10;
+            }
+            return a;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
